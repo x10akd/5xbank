a1e515465b382e7535f8bc612b179a15bf52969b
x10akd <x10akd@icloud.com>
2024-06-29 16:02:02 +0800

    [add]use url get info

--- frontend/src/BankSearch.jsx
+++ frontend/src/BankSearch.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 
 const BankSearch = () => {
@@ -10,7 +10,7 @@ const BankSearch = () => {
   const [branches, setBranches] = useState([]);
   const [selectedBranch, setSelectedBranch] = useState(null);
   const navigate = useNavigate();
-
+  const { bankCode: paramBankCode, branchCode, branchName } = useParams();
 
   useEffect(() => {
     const fetchBanks = async () => {
@@ -30,6 +30,48 @@ const BankSearch = () => {
     fetchBanks();
   }, []);
 
+  useEffect(() => {
+    const fetchBranches = async () => {
+      if (paramBankCode) {
+        try {
+          const response = await axios.get(
+            `http://127.0.0.1:5000/${paramBankCode}/branches/`
+          );
+          setBranches(
+            response.data.branches.map((branch) => ({
+              value: branch.branch_code,
+              label: branch.name,
+            }))
+          );
+          setBankCode(paramBankCode);
+        } catch (error) {
+          console.error("Error fetching branches:", error);
+        }
+      }
+    };
+
+    fetchBranches();
+  }, [paramBankCode]);
+
+  useEffect(() => {
+    const fetchBranchDetails = async () => {
+      if (paramBankCode && branchCode && branchName) {
+        try {
+          const response = await axios.get(
+            `http://127.0.0.1:5000/${paramBankCode}/${branchCode}/${encodeURIComponent(
+              branchName
+            )}.html`
+          );
+          setSelectedBranch(response.data);
+        } catch (error) {
+          console.error("Error fetching branch details:", error);
+        }
+      }
+    };
+
+    fetchBranchDetails();
+  }, [paramBankCode, branchCode, branchName]);
+
   const handleBankSelect = async (selectedOption) => {
     const selectedBankCode = selectedOption ? selectedOption.value : "";
     setBankCode(selectedBankCode);
@@ -46,6 +88,7 @@ const BankSearch = () => {
           }))
         );
         setSelectedBranch(null); // Clear previous branch selection
+        // navigate(`/${selectedBankCode}`);
       } catch (error) {
         console.error("Error fetching branches:", error);
       }
